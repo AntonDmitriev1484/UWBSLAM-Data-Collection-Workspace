cd91a26eedf31dac7997b70945d9faa1b15fbf7b
Anton Dmitriev <a.dmitriev@wustl.edu>
2025-06-21 11:04:51 -0500

    more organize

---
+++ kalibr/run_kalibr_camimu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Usage check
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <bag_filename>"
+    exit 1
+fi
+
+BAG_FILE="$1"
+
+rosrun kalibr kalibr_calibrate_cameras \
+    --bag "../collect/ros1/${BAG_FILE}" \
+    --target ./april_6x6_config.yaml \
+    --models pinhole-radtan pinhole-radtan \
+	--imu ../allan_var/imu_scaled.yaml \
+	--imu-models calibrated \
+    --topics /camera/camera/infra1/image_rect_raw /camera/camera/infra2/image_rect_raw
+

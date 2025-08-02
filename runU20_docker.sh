7a15e403ceb5d7c342eff7bc4fe65656e52a3789
AntonDmitriev1484 <71272032+AntonDmitriev1484@users.noreply.github.com>
2025-08-02 09:10:48 -0500

    many things

--- runU20_docker.sh
+++ runU20_docker.sh
@@ -2,6 +2,19 @@
 
 FOLDER=./
 xhost +local:root
-docker run -it -e "DISPLAY=$DISPLAY" -e "QT_X11_NO_MITSHM=1" \
+
+# Remove existing container
+docker rm -f U20 2>/dev/null
+
+# Start container in detached mode (-d)
+docker run -dit --name U20 -e "DISPLAY=$DISPLAY" -e "QT_X11_NO_MITSHM=1" \
     -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     -v "$FOLDER:/data" u20_tools:latest
+
+# Wait a bit to ensure container is fully started
+sleep 1
+
+# Launch two terminal windows and run docker exec
+gnome-terminal -- bash -c "docker exec -it U20 bash"
+gnome-terminal -- bash -c "docker exec -it U20 bash"
+
